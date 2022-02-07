58b9edab889bec2395b875c3dca02f82d877c0d6
a.andreyanov <a.andreyanov@sevstar.net>
2022-02-07 16:28:45 +0300

    working docker-compose

--- docker/db_init.sql
+++
@@ -1,3 +0,0 @@
-CREATE DATABASE magento /*\!40100 DEFAULT CHARACTER SET utf8 */;
-CREATE USER 'magento'@'localhost' IDENTIFIED BY 'password';
-GRANT ALL PRIVILEGES ON magento . * TO 'magento'@'localhost';
--- run.sh
+++ run.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-$PWD = pwd
 [ -d sources ] || git clone https://github.com/magento/magento2.git ./sources
-[ -d mysql ] || mkdir mysql
 
-docker build -f docker/Dockerfile ./docker -t magento2 &&
-docker rm -f magento2_dev &&
-docker run --name=magento2_dev -d -t -v $PWD/sources:/var/www/html/magento2 -v $PWD/mysql:/var/lib/mysql -p 8765:8765 magento2
+cd docker && docker-compose down && docker-compose build && docker-compose up
